64f6a6611b3c79501f840bf707eb022d5e4ee750
DESKTOP-4L2E8GR\10650 <glpat-iuYyX9FArytoVtK2rBre>
2023-02-20 23:43:44 +0900

    코딩테스트

---
+++ codingTest/level1/src/level1/Solution1.java
@@ -0,0 +1,11 @@
+package level1;
+
+public class Solution1 {
+
+	class Solution {
+	    public String solution(String[] cards1, String[] cards2, String[] goal) {
+	        String answer = "";
+	        return answer;
+	    }
+	}
+}
